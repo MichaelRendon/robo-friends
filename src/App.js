15c383064250e95f20caa317e9ea4d381ad41adb
MichaelRendon <49463393+MichaelRendon@users.noreply.github.com>
2019-05-13 17:58:55 -0400

    added json placeholder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
 import './App.css';
 
 class App extends React.Component {
 	constructor() {
 		super()
 		this.state = {
-		robots: robots,
+		robots: [],
 		searchfield: ''	
 		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response=> {
+			return response.json();
+		})
+		.then(users => {
+			this.setState({robots: users})
+		});
+		
 	}
 
 	onSearchChange = (event) => {
