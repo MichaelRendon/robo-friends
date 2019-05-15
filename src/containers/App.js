d616d5c46da9d6863433299622a1989733ae5014
MichaelRendon <49463393+MichaelRendon@users.noreply.github.com>
2019-05-14 21:17:52 -0400

    optimized build

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,23 +15,18 @@ class App extends React.Component {
 
 	componentDidMount() {
 		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response=> {
-			return response.json();
-		})
-		.then(users => {
-			this.setState({robots: users})
-		});
-		
+		.then(response=> response.json())
+		.then(users => {this.setState({robots: users})});
 	}
 
 	onSearchChange = (event) => {
 		this.setState({searchfield: event.target.value})
-		
 	}
 
 	render() {
-		const filteredRobots = this.state.robots.filter(robots => {
-		return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		const {robots, searchfield} = this.state;
+		const filteredRobots = robots.filter(robot => {
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 	})
 
 	return (
