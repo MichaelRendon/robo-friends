69a6e3bbb86fd355b8efa61e6c26260e9288be46
MichaelRendon <49463393+MichaelRendon@users.noreply.github.com>
2019-09-04 17:50:29 -0400

    Updates

---
+++ precache-manifest.bc0574cfb5b25ef355859941a12b9583.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7059b61e57d33fd46480dde42bf93814",
+    "url": "/MichaelRendon/robo-friends/index.html"
+  },
+  {
+    "revision": "8b26fca4efded57da1bb",
+    "url": "/MichaelRendon/robo-friends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "fb1dc4976f8ef813b89b",
+    "url": "/MichaelRendon/robo-friends/static/css/main.6f3f910f.chunk.css"
+  },
+  {
+    "revision": "8b26fca4efded57da1bb",
+    "url": "/MichaelRendon/robo-friends/static/js/2.39ec0b78.chunk.js"
+  },
+  {
+    "revision": "fb1dc4976f8ef813b89b",
+    "url": "/MichaelRendon/robo-friends/static/js/main.85dcb5c4.chunk.js"
+  },
+  {
+    "revision": "90e269c0d9add0378f99",
+    "url": "/MichaelRendon/robo-friends/static/js/runtime~main.2e822678.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/MichaelRendon/robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
